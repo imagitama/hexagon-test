2c4f43a08960de6e0809d36e9caebf0a65e3512a
Jared Williams <accounts@jaredwilliams.com.au>
2019-04-15 15:56:18 +0800

    Crude solution to the first input

--- src/components/calculator-output/formatter.ts
+++ src/components/calculator-output/formatter.ts
@@ -1,1 +1,68 @@
-export default (inputText: string) => 'ONE HUNDRED AND TWENTY THREE DOLLARS AND FORTY SIX CENTS'
+const numerals = {
+  million: 'million',
+  thousand: 'thousand',
+  hundred: 'hundred'
+}
+
+const AND = 'AND'
+const DOLLARS = 'DOLLARS'
+const CENTS = 'CENTS'
+
+const tensNames = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+// const teenNames = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+const digitNames = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+export default (inputText: string) => {
+  const inputRounded = parseFloat(inputText).toFixed(2)
+
+  const chunks = inputRounded.toString().split('.')
+  const dollarChunks = chunks[0].split('')
+  const centChunks = chunks[1].split('')
+
+  let result: string[] = []
+
+  dollarChunks.forEach((digit, idx) => {
+    if (dollarChunks.length === 1) {
+      result.push(digitNames[parseInt(digit) - 1])
+      return
+    }
+
+    if (idx === 0 && dollarChunks.length === 3) {
+      result.push(digitNames[parseInt(digit) - 1])
+      result.push(numerals.hundred)
+      return
+    }
+
+    if (idx === 1 && dollarChunks.length === 3) {
+      result.push(AND)
+      result.push(tensNames[parseInt(digit) - 2])
+      return
+    }
+
+    if (idx === 2 && dollarChunks.length === 3) {
+      result.push(digitNames[parseInt(digit) - 1])
+      result.push(DOLLARS)
+      return
+    }
+  })
+
+  centChunks.forEach((digit, idx) => {
+    if (idx === 0) {
+      result.push(AND)
+      result.push(tensNames[parseInt(digit) - 2])
+      return
+    }
+
+    if (idx === 1) {
+      result.push(digitNames[parseInt(digit) - 1])
+      result.push(CENTS)
+      return
+    }
+  })
+
+
+
+  return result.join(' ').toUpperCase()
+}
