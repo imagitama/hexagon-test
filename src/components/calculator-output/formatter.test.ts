ed21dcff73f7b0d9ba353162eaa5f577c5a85a2f
Jared Williams <accounts@jaredwilliams.com.au>
2019-04-15 16:03:31 +0800

    Write out some scenarios

--- src/components/calculator-output/formatter.test.ts
+++ src/components/calculator-output/formatter.test.ts
@@ -1,9 +1,16 @@
 import formatter from './formatter'
 
 const inputsAndExpectedOutputs = [
+  // Original scenario - round up
   ['123.456', 'ONE HUNDRED AND TWENTY THREE DOLLARS AND FORTY SIX CENTS'],
+
+  // Round down
   ['987.654', 'NINE HUNDRED AND EIGHTY SEVEN DOLLARS AND SIXTY FIVE CENTS'],
+
+  // Two digits for dollars and double 0s for cents
   ['10.00', 'TEN DOLLARS'],
+
+  // Single digit for dollars and single digit for cents
   ['5.1', 'FIVE DOLLARS AND TEN CENTS']
 ]
 
